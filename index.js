0f579ad9795be0c6f7cab65c8f560a8af40f4e33
Domenico Giambra <zorinik@gmail.com>
2025-09-29 00:23:48 +0200

    Update version to 1.2.1 and export new context modules

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ import Agent from "./Agent.js";
 import Thread from "./Thread.js";
 import Tool from "./Tool.js";
 import Logger from "./Logger.js";
+
 import ContextHandler from "./ContextHandler.js";
 import Summarizer from "./Summarizer.js";
+
+import Context from "./Context.js";
+import File from "./Contexts/File.js";
+import Text from "./Contexts/Text.js";
 
 export {
 	Symposium,
@@ -16,4 +21,7 @@ export {
 	Logger,
 	ContextHandler,
 	Summarizer,
+	Context,
+	File,
+	Text,
 };
