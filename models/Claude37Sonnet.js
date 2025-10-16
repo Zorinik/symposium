917afb0372846120164c082105ed4e666031f88f
Domenico <zorinik@gmail.com>
2025-10-16 17:31:37 +0200

    add Claude37Sonnet class and Claude45Haiku class; bump version to 1.3.5

--- models/Claude37Sonnet.js
+++ models/Claude37Sonnet.js
@@ -1,6 +1,6 @@
 import AnthropicModel from "./AnthropicModel.js";
 
-export default class Claude35Sonnet extends AnthropicModel {
+export default class Claude37Sonnet extends AnthropicModel {
 	name = 'claude-3-7-sonnet-latest';
 	label = 'claude-3.7-sonnet';
 	tokens = 200000;
