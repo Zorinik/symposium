b28c3d405b1fdd8053ab365f60e53e54b29282bb
Domenico Giambra <zorinik@gmail.com>
2026-08-20 15:35:28 +0200

    openrouter test

---
+++ Models/OpenRouterModel.js
@@ -0,0 +1,32 @@
+import LegacyOpenAIModel from "./LegacyOpenAIModel.js";
+import OpenAI from "openai";
+
+export default class OpenRouterModel extends LegacyOpenAIModel {
+	async getModels() {
+		return new Map([
+			['qwen-3.8', {
+				name: 'qwen/qwen3.8-27b',
+				tokens: 1000000,
+			}],
+			['deepseek-4-flash', {
+				name: 'deepseek/deepseek-v4-flash-0731',
+				tokens: 1000000,
+			}],
+			['deepseek-4-pro', {
+				name: 'deepseek/deepseek-v4-pro-0813',
+				tokens: 1000000,
+			}],
+		]);
+	}
+
+	getOpenAi() {
+		if (!this.openai) {
+			this.openai = new OpenAI({
+				baseURL: 'https://openrouter.ai',
+				apiKey: process.env.OPENROUTER_API_KEY,
+			});
+		}
+
+		return this.openai;
+	}
+}
